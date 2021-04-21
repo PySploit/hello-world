e7bdd993fa7cff33a7560b5da44741db219299a4
PySploit <dgravins27@gmail.com>
2021-04-21 20:49:03 +0300

    adding files

--- myblog/settings.py
+++ myblog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6l2d%r()odkk_-#+$2o6f9r6wpmbs4mp8rx1)*rme6qx@vdiw*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://soulines-django.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
